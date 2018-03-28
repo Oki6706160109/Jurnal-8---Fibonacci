1af44c9a39c840986ee2f91fa440501f3e339ec9
Oki Ariyan Kartiko Aji <37764221+Oki6706160109@users.noreply.github.com>
2018-03-28 11:34:34 +0700

    UPDATE
    now the result will be directly from the number in the input of fibonacci number

--- JURNAL8/1a/Fibonacci/src/fibonacci/Fibonacci.java
+++ JURNAL8/1a/Fibonacci/src/fibonacci/Fibonacci.java
@@ -5,28 +5,29 @@ import java.util.Scanner;
 
 public class Fibonacci {
 
-   public static void main(String[] args) {
-       Scanner s = new Scanner(System.in);
-       System.out.print("Masukkan Angka : ");
-       int n = s.nextInt();
-       fibonacci(n);
-   }
-
-   public static void fibonacci(int n) {
-       if (n == 0) {
-           System.out.println("0");
-       } else if (n == 1) {
-           System.out.println("0 1");
-       } else {
-           System.out.print("0 1 ");
-           int a = 0;
-           int b = 1;
-           for (int i = 1; i < n; i++) {
-               int nextNumber = a + b;
-               System.out.print(nextNumber + " ");
-               a = b;
-               b = nextNumber;
-           }
-       }
-   }
+   public static void main(String args[]) {
+ 
+	log("Masukkan Angka : ");
+	int number = new Scanner(System.in).nextInt();
+        log("\nNomor Fibonacci dari " + number + " adalah " + (fibonacciLoop(number) + ""));
+	}
+	public static int fibonacciLoop(int number) {
+		if (number == 1 || number == 2) {
+			return 1;
+		}
+		int fibo1 = 1, fibo2 = 1, fibonacci = 1;
+		for (int i = 3; i <= number; i++) {
+			fibonacci = fibo1 + fibo2;
+			fibo1 = fibo2;
+			fibo2 = fibonacci;
+ 
+		}
+		return fibonacci;
+	}
+ 
+	private static void log(String number) {
+		System.out.println(number);
+ 
+	}
+ 
 }
